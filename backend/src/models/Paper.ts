360e286e29de9cd293f9a78e44aa4c3a742245bb
Elfo <elfo.here@gmail.com>
2024-12-27 19:55:27 +0100

    Update Paper model - add important fields

--- backend/src/models/Paper.ts
+++ backend/src/models/Paper.ts
@@ -8,18 +8,33 @@ export interface IPaper extends Document {
     final_submission: boolean;
     user: mongoose.Schema.Types.ObjectId;
     category: mongoose.Schema.Types.ObjectId;
+    conference: mongoose.Schema.Types.ObjectId; // Link to conference
+    abstract: string; // New field for paper abstract
+    keywords: string[]; // New field for paper keywords
+    authors: { firstName: string; lastName: string }[]; // New field for additional authors
     created_at: Date;
     updated_at: Date;
 }
 
 const PaperSchema: Schema = new Schema({
     title: { type: String, required: true },
-    status: { type: String, required: true },
+    status: {
+        type: String,
+        required: true,
+        enum: ['draft', 'submitted', 'under review', 'accepted', 'rejected'], // Aligning with CMS rules
+    },
     submission_date: { type: Date, required: true },
     file_link: { type: String, required: true },
     final_submission: { type: Boolean, default: false },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },  // Student
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },  // References a global category
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Student
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },  // References a global category
+    conference: { type: mongoose.Schema.Types.ObjectId, ref: 'Conference', required: true }, // Link to associated conference
+    abstract: { type: String, required: true }, // Abstract field
+    keywords: { type: [String], required: true }, // Array of keywords
+    authors: [{
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true }
+    }], // Authors details
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
 });
